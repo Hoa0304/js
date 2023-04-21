5a8b753602615d0159b1567e333a0b6713c46399
hoa tran thi cam <tranthicamhoa2k4@gmail.com>
2023-04-21 12:03:03 +0700

    first

--- part1/js/variableScope.js
+++ part1/js/variableScope.js
@@ -1,1 +1,16 @@
 //------------Phạm vi của biến ------------------
+var name  = 'Hoa';
+function getName(){
+    console.log('getName: '+ name);
+}
+
+getName();
+console.log('name: '+name);
+//--------------
+function getname(){
+    var name = 'Man';
+    console.log('getName: '+ name);
+}
+
+getname(); // Man
+console.log('name: '+name); //Hoa
